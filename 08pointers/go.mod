0dad9d33050e98066377f7a421577d279f075d7c
abinth11 <abinth250@gmail.com>
2024-05-08 01:38:08 +0530

    pointers

---
+++ 08pointers/go.mod
@@ -0,0 +1,3 @@
+module pointersex
+
+go 1.22.2
---
+++ 08pointers/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ptr1 *int
+	fmt.Println("Ptr 1=", ptr1)
+
+	randomNumber := 30
+	ptr2 := &randomNumber
+	fmt.Println("Ptr 2=", ptr2)
+	fmt.Println("Val of Ptr 2=", *ptr2)
+	*ptr2 = (*ptr2) * (*ptr2)
+	*ptr2 = *ptr2 + 1
+	fmt.Println("Final val of Ptr 2=", *ptr2)
+
+}
