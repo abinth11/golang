e09a75959eb4fe8a602a3df0ef62313402ad146b
abinth11 <abinth250@gmail.com>
2024-05-08 01:09:07 +0530

    second commit

--- 06timefunctions/main.go
+++ 06timefunctions/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hii")
+	currentTime := time.Now()
+	fmt.Println("Current time= ", currentTime)
+
+	formattedTime := currentTime.Format("01-02-2006 15:04:05 Monday")
+	fmt.Println("Formatted time= ", formattedTime)
+
+	randomDate := time.Date(2020, time.November, 12, 23, 23, 0, 0, time.UTC)
+	fmt.Println("Current Date= ", randomDate)
+
+	formattedDate := randomDate.Format("01-02-2006")
+	fmt.Println("Formatted Date= ", formattedDate)
 }
