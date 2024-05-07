018c3d01e1ad26c776051548cc90b6840bf57e50
abinth11 <abinth250@gmail.com>
2024-05-08 01:08:33 +0530

    second commit

---
+++ 07Executables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+	//*  go env => for environment
+	/*
+	* MAC => GOOS="darwin" go build
+	* WINDOWS => GOOS="windows" go build
+	* LINUX => GOOS="linux" go build
+	 */
+}
