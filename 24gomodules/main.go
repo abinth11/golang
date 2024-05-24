48f4b4699fc71f75cce1f9dba49c2a5f68905bbc
abinth11 <abinth250@gmail.com>
2024-05-24 19:35:41 +0530

    chapter modules

---
+++ 24gomodules/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+/*
+commands
+go mod tidy
+go list
+go list all
+go get -u github.com/gorilla/mux
+go mod verify
+go mod init
+go list -m all
+go run filename.go
+go build path
+go list -m -versions github.com/gorilla/mux
+go mod why github.com/gorilla/mux
+go mod graph
+go mod edit -go 1.20
+go mod edit -module
+go mod vendor
+go run -mod=vendor main.go
+*/
+
+func main() {
+	fmt.Println("Server example")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HandleHome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Response from server</h1>"))
+}
